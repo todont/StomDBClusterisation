065d5443503dd707c52c6f6bb6e2332794c14b40
Pavel <panarin-2000@list.ru>
2021-10-15 15:09:02 +0300

    Add TaskOrder class.

---
+++ ClusterSotmDB/TaskOrder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ClusterSotmDB
+{
+    class TaskOrder
+    {
+        public readonly uint id;
+        public TaskOrder(uint i)
+        {
+            id = i;
+        }
+        private SortedSet<uint> services;
+        public void AddServiceById(uint i)
+        {
+            services.Add(i);
+        }
+        public bool GetServiceById(uint i)
+        {
+            return services.Contains(i);
+        }
+
+    }
+}
